bec6d19d3c6d4b29538d7112ed23d1f3f345b55d
Michael Caley <mjcaley@darkarctic.com>
2026-07-06 07:25:11 -0400

    Adding test for prelude

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -45,3 +45,39 @@ def test_version_yields_section():
     assert len(sections[1].notes) == 2
     assert sections[1].notes[0].note == "note1"
     assert sections[1].notes[1].note == "note2"
+
+
+def test_version_with_prelude():
+    class MockLoader:
+        def __getitem__(self, version):
+            return [("file1", b"sha1")]
+
+        def parse_note_file(self, filename, sha):
+            return {"prelude": ["note0"], "singlenote": ["note0"]}
+        
+        @property
+        def versions(self):
+            return ["v1.0.0"]
+
+    class MockConfig:
+        prelude_section_name = "prelude"
+        sections = [
+            FakeSection("prelude", None),
+            FakeSection("singlenote", "Single Note Section"),
+        ]
+
+    loader = MockLoader()
+    config = MockConfig()
+    version = RenoVersion(loader, config, "v1.0.0")
+
+    sections = list(version.sections())
+
+    assert len(sections) == 2
+
+    assert sections[0].name == "prelude"
+    assert len(sections[0].notes) == 1
+    assert sections[0].notes[0].note == "note0"
+
+    assert sections[1].name == "singlenote"
+    assert len(sections[1].notes) == 1
+    assert sections[1].notes[0].note == "note0"
